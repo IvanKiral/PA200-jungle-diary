3ae59c8ef59ee38730901d1c1d48086afe8e4ef9
Ivan Kiral <kiralivan@gmail.com>
2023-05-11 17:15:10 +0200

    fix toggle error in console

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -21,7 +21,7 @@ export const Toggle: FC<ToggleProps> = ({ text }) => {
 					type="checkbox"
 					value=""
 					checked={toggled}
-					onClick={() => setToggled(prev => !prev)}
+					onChange={() => setToggled(prev => !prev)}
 					className="sr-only peer m-0"
 				/>
 				<div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-emerald-300 dark:peer-checked:bg-emerald-500 dark:md:peer-checked:bg-emerald-600" />
