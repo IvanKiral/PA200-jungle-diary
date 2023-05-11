d7315d80ca73a353b88d9a192aab5bcf318ad96b
Diana Janickova <janickova.diana@gmail.com>
2023-05-11 11:01:51 +0200

    fix: edit util functions

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -22,3 +22,12 @@ export const months = [
 	'November',
 	'December'
 ];
+
+export const defaultToday = () => {
+	const today = new Date();
+	const formattedDate = `${today.getFullYear()}-${String(
+		today.getMonth() + 1
+	).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+
+	return formattedDate;
+};
