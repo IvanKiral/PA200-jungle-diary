3a62a7076791c23c989195e44d517a9872301700
Ivan Kiral <kiralivan@gmail.com>
2023-05-11 16:38:03 +0200

    fix task bars not showing icons

--- src/components/TaskFlag.tsx
+++ src/components/TaskFlag.tsx
@@ -12,20 +12,22 @@ type TaskFlagProps = {
 
 export const TaskFlag: FC<TaskFlagProps> = props => {
 	let styles = 'bg-blue-300 text-blue-900';
-	let icon = <WaterIcon className="w-5 h-5 fill-blue-900" />;
+	let icon = <WaterIcon className="w-5 h-5 fill-blue-900 overflow-visible" />;
 	if (props.type === 'repot') {
 		styles = 'bg-amber-200 text-yellow-700';
-		icon = <PlantPotIcon className="w-5 h-5 fill-yellow-700" />;
+		icon = (
+			<PlantPotIcon className="w-5 h-5 fill-yellow-700 overflow-visible" />
+		);
 	} else if (props.type === 'fertilize') {
 		styles = 'bg-red-300 text-red-700';
-		icon = <FertilizeIcon className="w-5 h-5 fill-red-700" />;
+		icon = <FertilizeIcon className="w-5 h-5 fill-red-700 overflow-visible" />;
 	}
 	return (
 		<div
-			className={`${styles} ${props.extraClasses} rounded-sm mb-[2px] truncate ps-1 flex flex-row items-center gap-1"`}
+			className={`${styles} ${props.extraClasses} rounded-sm mb-[2px] ps-1 flex flex-row items-center gap-1`}
 		>
 			{icon}
-			{props.text}
+			<p className="truncate">{props.text}</p>
 		</div>
 	);
 };
