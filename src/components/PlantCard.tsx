2b1a06062464e414879634798bffacae607c614b
Diana Janickova <janickova.diana@gmail.com>
2023-05-15 20:16:55 +0200

    fix: add unique key to PlantCard

--- src/components/PlantCard.tsx
+++ src/components/PlantCard.tsx
@@ -32,7 +32,7 @@ const formatDate = (date: string) => {
 
 export const PlantCard: FC<PlantCardProps> = ({ plant }) => (
 	<div
-		key={plant.data.name}
+		key={plant.id}
 		className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 z-0"
 	>
 		<div
