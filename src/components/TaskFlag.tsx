e13279a0fb87c4fe87cf74f10a06dbe96f507cc1
Ivan Kiral <kiralivan@gmail.com>
2023-05-16 22:22:01 +0200

    Update tasks colors

--- src/components/TaskFlag.tsx
+++ src/components/TaskFlag.tsx
@@ -14,13 +14,13 @@ export const TaskFlag: FC<TaskFlagProps> = props => {
 	let styles = 'bg-blue-300 text-blue-900';
 	let icon = <WaterIcon className="w-5 h-5 fill-blue-900 overflow-visible" />;
 	if (props.type === 'repot') {
+		styles = 'bg-red-300 text-red-700';
+		icon = <PlantPotIcon className="w-5 h-5 fill-red-700 overflow-visible" />;
+	} else if (props.type === 'fertilize') {
 		styles = 'bg-amber-200 text-yellow-700';
 		icon = (
-			<PlantPotIcon className="w-5 h-5 fill-yellow-700 overflow-visible" />
+			<FertilizeIcon className="w-5 h-5 text-yellow-700 overflow-visible" />
 		);
-	} else if (props.type === 'fertilize') {
-		styles = 'bg-red-300 text-red-700';
-		icon = <FertilizeIcon className="w-5 h-5 fill-red-700 overflow-visible" />;
 	}
 	return (
 		<div
