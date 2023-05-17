ba9eb5036241dbf5b614ce3a6cd2380c0d7f6320
Ivan Kiral <kiralivan@gmail.com>
2023-05-17 18:45:43 +0200

    fix and refactor calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,7 +1,9 @@
-import { Dayjs } from 'dayjs';
-import { FC, ReactElement } from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+import { FC, ReactElement, useCallback, useMemo } from 'react';
 
 import { months, daysInWeek } from '../utils/dateUtils';
+import { range } from '../utils/listUtils';
+import { PlantType } from '../types/PlantType';
 
 import { ArrowIcon } from './icons/ArrowIcon';
 import { TaskFlag } from './TaskFlag';
@@ -14,20 +16,159 @@ export type CalendarDaysWithReminders = Record<
 >;
 
 type CalendarProps = {
-	previousMonthDays: number[];
-	currentMonthDays: CalendarDaysWithReminders;
-	nextMonthDays: number[];
+	userPlants: PlantType[];
 	selectedDate: Dayjs;
 	setSelectedDate: (date: Dayjs) => void;
 };
 
 export const Calendar: FC<CalendarProps> = ({
-	previousMonthDays,
-	currentMonthDays,
-	nextMonthDays,
+	userPlants,
 	selectedDate,
 	setSelectedDate
 }) => {
+	const getDaysToDisplay = useCallback(() => {
+		const previousMonth = selectedDate.subtract(1, 'month');
+
+		const firstDayInMonth =
+			selectedDate.startOf('month').day() === 0
+				? 7
+				: selectedDate.startOf('month').day();
+
+		const previousMonthDays = range(
+			previousMonth.daysInMonth() - (firstDayInMonth - 1) + 1,
+			previousMonth.daysInMonth()
+		);
+
+		const thisMonthRange = Object.fromEntries(
+			range(1, selectedDate.daysInMonth()).map(num => [
+				num,
+				{
+					water: [] as string[],
+					repot: [] as string[],
+					fertilize: [] as string[]
+				}
+			])
+		);
+		const nextMonthDays = range(
+			1,
+			7 - ((selectedDate.daysInMonth() + previousMonthDays.length) % 7)
+		);
+
+		return { previousMonthDays, thisMonthRange, nextMonthDays };
+	}, [selectedDate]);
+
+	const calculateIntervalStart = useCallback(
+		(nextTaskDate: Dayjs, interval: number) => {
+			const startOfSelectedMonth = selectedDate.startOf('month');
+			const numberOfDays = startOfSelectedMonth.diff(nextTaskDate, 'day');
+			const intervals_remainder = numberOfDays % interval;
+			const number_of_intervals =
+				intervals_remainder > 0
+					? Math.trunc(numberOfDays / interval) + 1
+					: Math.trunc(numberOfDays / interval);
+
+			return selectedDate.month() === nextTaskDate.month() &&
+				selectedDate.year() === nextTaskDate.year()
+				? nextTaskDate
+				: nextTaskDate.add(number_of_intervals * interval, 'day');
+		},
+		[selectedDate]
+	);
+
+	const calculateDays = (startDay: Dayjs, endDay: Dayjs, interval: number) => {
+		if (startDay.unix() >= endDay.unix()) {
+			return [];
+		}
+
+		let d = startDay.date();
+		const result = [];
+		while (d <= endDay.date()) {
+			result.push(d);
+			d += interval;
+		}
+		return result;
+	};
+
+	const calculateTaskDaysDate = (nextTaskDate: Dayjs, taskInterval: number) => {
+		const endOfSelectedMonth = selectedDate.endOf('month').endOf('day');
+
+		const interval_start =
+			endOfSelectedMonth.unix() < nextTaskDate.unix()
+				? nextTaskDate
+				: calculateIntervalStart(nextTaskDate, taskInterval);
+
+		return calculateDays(interval_start, endOfSelectedMonth, taskInterval);
+	};
+
+	const calculateReminders = useCallback(() => {
+		const currentDate = dayjs();
+		const result = getDaysToDisplay();
+
+		if (
+			selectedDate.month() < currentDate.month() &&
+			selectedDate.year() <= currentDate.year()
+		) {
+			return result;
+		}
+
+		userPlants.map(plant => {
+			const nextWaterDate = dayjs(plant.nextWater);
+			const nextFertilizeDate = dayjs(plant.nextFertilize);
+			const nextRepotDate = dayjs(plant.nextRepot);
+
+			if (nextWaterDate.unix() < currentDate.unix()) {
+				if (
+					selectedDate.month() === nextWaterDate.month() &&
+					selectedDate.year() === currentDate.year()
+				) {
+					result.thisMonthRange[currentDate.date()].water.push(
+						`${plant.name} - overdue`
+					);
+				}
+			} else {
+				calculateTaskDaysDate(nextWaterDate, plant.waterInterval).map(i =>
+					result.thisMonthRange[i].water.push(plant.name)
+				);
+			}
+
+			if (
+				nextFertilizeDate.unix() < currentDate.unix() &&
+				selectedDate.year() === currentDate.year()
+			) {
+				if (selectedDate.month() === nextFertilizeDate.month()) {
+					result.thisMonthRange[currentDate.date()].fertilize.push(
+						`${plant.name} - overdue`
+					);
+				}
+			} else {
+				calculateTaskDaysDate(nextFertilizeDate, plant.fertilizeInterval).map(
+					i => result.thisMonthRange[i].fertilize.push(plant.name)
+				);
+			}
+
+			if (nextRepotDate.unix() < currentDate.unix()) {
+				if (
+					selectedDate.month() === nextRepotDate.month() &&
+					selectedDate.year() === currentDate.year()
+				) {
+					result.thisMonthRange[currentDate.date()].repot.push(
+						`${plant.name} - overdue`
+					);
+				}
+			} else {
+				calculateTaskDaysDate(nextRepotDate, plant.repotInterval).map(i =>
+					result.thisMonthRange[i].repot.push(plant.name)
+				);
+			}
+		});
+		return result;
+	}, [selectedDate, getDaysToDisplay, calculateIntervalStart, userPlants]);
+
+	const calendarDaysWithReminders = useMemo(
+		() => calculateReminders(),
+		[selectedDate, calculateReminders]
+	);
+
 	const renderDisabledCalendarWindow = (day: number, month: number) => (
 		<div
 			key={`${month}-${day}`}
@@ -72,49 +213,51 @@ export const Calendar: FC<CalendarProps> = ({
 					</p>
 				))}
 
-				{previousMonthDays.map(day =>
+				{calendarDaysWithReminders.previousMonthDays.map(day =>
 					renderDisabledCalendarWindow(
 						day,
 						selectedDate.subtract(1, 'month').month()
 					)
 				)}
 
-				{Object.entries(currentMonthDays).map(([day, tasks]) => (
-					<div
-						key={`${selectedDate.month()}-${day}`}
-						className="border min-h-[124px] p-1"
-					>
-						<div className="w-full">
-							<p>{day}</p>
-						</div>
+				{Object.entries(calendarDaysWithReminders.thisMonthRange).map(
+					([day, tasks]) => (
+						<div
+							key={`${selectedDate.month()}-${day}`}
+							className="border min-h-[124px] p-1"
+						>
+							<div className="w-full">
+								<p>{day}</p>
+							</div>
 
-						<div className="pt-1">
-							{tasks.water.map(plantName => (
-								<TaskFlag
-									key={`water-${plantName}-${day}`}
-									type="water"
-									text={plantName}
-								/>
-							))}
-							{tasks.repot.map(plantName => (
-								<TaskFlag
-									key={`repot-${plantName}-${day}`}
-									type="repot"
-									text={plantName}
-								/>
-							))}
-							{tasks.fertilize.map(plantName => (
-								<TaskFlag
-									key={`fertilize-${plantName}-${day}`}
-									type="fertilize"
-									text={plantName}
-								/>
-							))}
+							<div className="pt-1">
+								{tasks.water.map(plantName => (
+									<TaskFlag
+										key={`water-${plantName}-${day}`}
+										type="water"
+										text={plantName}
+									/>
+								))}
+								{tasks.repot.map(plantName => (
+									<TaskFlag
+										key={`repot-${plantName}-${day}`}
+										type="repot"
+										text={plantName}
+									/>
+								))}
+								{tasks.fertilize.map(plantName => (
+									<TaskFlag
+										key={`fertilize-${plantName}-${day}`}
+										type="fertilize"
+										text={plantName}
+									/>
+								))}
+							</div>
 						</div>
-					</div>
-				))}
+					)
+				)}
 
-				{nextMonthDays.map(day =>
+				{calendarDaysWithReminders.nextMonthDays.map(day =>
 					renderDisabledCalendarWindow(
 						day,
 						selectedDate.add(1, 'month').month()
--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,8 +1,7 @@
-import dayjs, { Dayjs } from 'dayjs';
-import { FC, useCallback, useEffect, useMemo, useState } from 'react';
+import dayjs from 'dayjs';
+import { FC, useEffect, useState } from 'react';
 
 import { Calendar } from '../components/Calendar';
-import { range } from '../utils/listUtils';
 import { useUser } from '../hooks/useUser';
 import { PlantType } from '../types/PlantType';
 import { fetchUserPlants } from '../repository/fetchUserPlants';
@@ -22,151 +21,12 @@ export const CalendarPage: FC = () => {
 		getUserPlants();
 	}, [user]);
 
-	const getDaysToDisplay = useCallback(() => {
-		const previousMonth = selectedDate.subtract(1, 'month');
-
-		const firstDayInMonth =
-			selectedDate.startOf('month').day() === 0
-				? 7
-				: selectedDate.startOf('month').day();
-
-		const previousMonthDays = range(
-			previousMonth.daysInMonth() - (firstDayInMonth - 1) + 1,
-			previousMonth.daysInMonth()
-		);
-
-		const thisMonthRange = Object.fromEntries(
-			range(1, selectedDate.daysInMonth()).map(num => [
-				num,
-				{
-					water: [] as string[],
-					repot: [] as string[],
-					fertilize: [] as string[]
-				}
-			])
-		);
-		const nextMonthDays = range(
-			1,
-			7 - ((selectedDate.daysInMonth() + previousMonthDays.length) % 7)
-		);
-
-		return { previousMonthDays, thisMonthRange, nextMonthDays };
-	}, [selectedDate]);
-
-	const calculate_interval_start = useCallback(
-		(nextTaskDate: Dayjs, interval: number) => {
-			const startOfSelectedMonth = selectedDate.startOf('month');
-			const numberOfDays = startOfSelectedMonth.diff(nextTaskDate, 'day');
-			const intervals_remainder = numberOfDays % interval;
-			const number_of_intervals =
-				intervals_remainder > 0
-					? Math.trunc(numberOfDays / interval) + 1
-					: Math.trunc(numberOfDays / interval);
-
-			return selectedDate.month() === nextTaskDate.month()
-				? nextTaskDate
-				: nextTaskDate.add(number_of_intervals * interval, 'day');
-		},
-		[selectedDate]
-	);
-
-	const calculateDays = (
-		startDay: number,
-		endDay: number,
-		interval: number
-	) => {
-		let d = startDay;
-		const result = [];
-		while (d <= endDay) {
-			result.push(d);
-			d += interval;
-		}
-		return result;
-	};
-
-	const calculateReminders = useCallback(() => {
-		const currentDate = dayjs();
-		const result = getDaysToDisplay();
-
-		if (
-			selectedDate.month() < currentDate.month() &&
-			selectedDate.year() <= currentDate.year()
-		) {
-			return result;
-		}
-
-		userPlants.map(plant => {
-			const nextWaterDate = dayjs(plant.nextWater);
-			const nextFertilizeDate = dayjs(plant.nextFertilize);
-			const nextRepotDate = dayjs(plant.nextRepot);
-
-			if (nextWaterDate.unix() < currentDate.unix()) {
-				if (selectedDate.month() === nextWaterDate.month()) {
-					result.thisMonthRange[currentDate.date()].water.push(plant.name);
-				}
-			} else {
-				const interval_start = calculate_interval_start(
-					nextWaterDate,
-					plant.waterInterval
-				);
-				const endOfSelectedMonth = selectedDate.endOf('month');
-				calculateDays(
-					interval_start.date(),
-					endOfSelectedMonth.date(),
-					plant.waterInterval
-				).map(i => result.thisMonthRange[i].water.push(plant.name));
-			}
-
-			if (nextFertilizeDate.unix() < currentDate.unix()) {
-				if (selectedDate.month() === nextFertilizeDate.month()) {
-					result.thisMonthRange[currentDate.date()].fertilize.push(plant.name);
-				}
-			} else {
-				const interval_start = calculate_interval_start(
-					nextFertilizeDate,
-					plant.fertilizeInterval
-				);
-				const endOfSelectedMonth = selectedDate.endOf('month');
-				calculateDays(
-					interval_start.date(),
-					endOfSelectedMonth.date(),
-					plant.fertilizeInterval
-				).map(i => result.thisMonthRange[i].fertilize.push(plant.name));
-			}
-
-			if (nextRepotDate.unix() < currentDate.unix()) {
-				if (selectedDate.month() === nextRepotDate.month()) {
-					result.thisMonthRange[currentDate.date()].repot.push(plant.name);
-				}
-			} else {
-				const interval_start = calculate_interval_start(
-					nextRepotDate,
-					plant.repotInterval
-				);
-				const endOfSelectedMonth = selectedDate.endOf('month');
-				calculateDays(
-					interval_start.date(),
-					endOfSelectedMonth.date(),
-					plant.repotInterval
-				).map(i => result.thisMonthRange[i].repot.push(plant.name));
-			}
-		});
-		return result;
-	}, [selectedDate, getDaysToDisplay, calculate_interval_start, userPlants]);
-
-	const calendarDaysWithReminders = useMemo(
-		() => calculateReminders(),
-		[selectedDate, calculateReminders]
-	);
-
 	return (
 		<div className="w-full">
 			<Calendar
 				selectedDate={selectedDate}
 				setSelectedDate={date => setSelectedDate(date)}
-				previousMonthDays={calendarDaysWithReminders.previousMonthDays}
-				currentMonthDays={calendarDaysWithReminders.thisMonthRange}
-				nextMonthDays={calendarDaysWithReminders.nextMonthDays}
+				userPlants={userPlants}
 			/>
 		</div>
 	);
